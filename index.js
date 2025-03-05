3424071620d4fd106de527de678f62642eab84e6
OffScript <0ffxscrjpt@gmail.com>
2025-03-06 01:30:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["DanTuBao Collabs With Lunar Royal", "Sharkie"];
+const statusMessages = ["Bạn Có Đói hok?","Đang Cooking Server","Thời Gian Ra Mắt? Tôi Cũng Chịu =)","How Tu ét","DanTuBao Collabs With Lunar Royal", "Sharkie","RealTime©"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
